6370124e93b052dcc04fd8d4f39431a9538fb512
powerdev73 <chaosblue@naver.com>
2021-09-08 19:58:18 +0900

    class 태그 변경

--- components/NavigationComp.jsx
+++ components/NavigationComp.jsx
@@ -2,13 +2,13 @@ import React from "react";
 const NavigationComp = () => {
   return (
     <>
-      <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#" style={{ fontWeight: "bolder" }}>
+      <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#" style={{ fontWeight: "bolder" }}>
             측정시험페이지
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNavDropdown"
@@ -16,36 +16,36 @@ const NavigationComp = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/projectTest1">
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/projectTest1">
                   시험항목 #1
                 </a>
               </li>
 
-              <li class="nav-item">
-                <a class="nav-link " href="/projectTest2">
+              <li className="nav-item">
+                <a className="nav-link " href="/projectTest2">
                   시험항목 #2
                 </a>
               </li>
 
-              <li class="nav-item">
-                <a class="nav-link " href="/projectTest3">
+              <li className="nav-item">
+                <a className="nav-link " href="/projectTest3">
                   시험항목 #3
                 </a>
               </li>
 
-              <li class="nav-item">
-                <a class="nav-link " href="/projectTest4">
+              <li className="nav-item">
+                <a className="nav-link " href="/projectTest4">
                   시험항목 #4
                 </a>
               </li>
 
-              <li class="nav-item">
-                <a class="nav-link " href="/projectTest5">
+              <li className="nav-item">
+                <a className="nav-link " href="/projectTest5">
                   시험항목 #5
                 </a>
               </li>
